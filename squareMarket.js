0463186072940de5b24c904dd805e1c5074d3761
Adam-walker <quarterque@gmail.com>
2019-01-02 22:15:39 -0600

    remodify code of shelf

---
+++ squareMarket.js
@@ -0,0 +1,26 @@
+$(document).ready(()=>{
+    $('#cart').on('mouseover', ()=>{
+      $('#cartMenu').show();
+    })
+      $('#cart').on('mouseleave', ()=>{
+      $('#cartMenu').hide();
+    })
+  });
+  
+  $(document).ready(()=>{
+    $('#account').on('mouseover', ()=>{
+      $('#accountMenu').show();
+    })
+      $('#account').on('mouseleave', ()=>{
+      $('#accountMenu').hide();
+    })
+  })
+  
+  $(document).ready(()=>{
+    $('#help').on('mouseover', ()=>{
+      $('#helpMenu').show();
+    })
+      $('#help').on('mouseleave', ()=>{
+      $('#helpMenu').hide();
+    })
+  })
